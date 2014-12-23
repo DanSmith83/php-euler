426f70d1856a72e3d7b01d60db360c062ca3f2e3
Dan Smith <dan@walmleyguitar.co.uk>
2014-12-23 13:36:30 +0000

    Archiving files

---
+++ src/ArchiveCommand.php
@@ -0,0 +1,54 @@
+<?php namespace Euler;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ArchiveCommand extends Command {
+
+    public function configure()
+    {
+        $this->setName('archive')
+             ->setDescription('Archive files');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $file  = 'archive.zip';
+        $zip   = new \ZipArchive;
+
+        $zip->open($file, \ZipArchive::CREATE);
+
+        if ( ! $zip->open($file, \ZipArchive::OVERWRITE))
+        {
+
+        }
+
+        $this->zipDirectory($zip, $this->getApplication()->config['functions_directory']);
+        $this->zipDirectory($zip, $this->getApplication()->config['resources_directory']);
+        $this->zipDirectory($zip, $this->getApplication()->config['solutions_directory']);
+
+        if ( ! $zip->status == \ZipArchive::ER_OK)
+        {
+
+        }
+
+        $zip->close();
+    }
+
+    /**
+     * @param $zip
+     * @param $directory
+     * @return mixed
+     */
+    public function zipDirectory($zip, $directory)
+    {
+        if (is_dir($directory))
+        {
+            foreach (glob($directory . '/*') as $file)
+            {
+                $zip->addFile($file, sprintf('%s/%s', $directory, $file));
+            }
+        }
+    }
+}
