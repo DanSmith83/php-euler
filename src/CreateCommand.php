7a6c5972e5c5d40416c10bba51c3581924414f6c
Dan Smith <dan@walmleyguitar.co.uk>
2014-12-23 08:52:58 +0000

    Renamed command

--- src/CreateCommand.php
+++ src/CreateCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 
 
-class SetupCommand extends Command {
+class CreateCommand extends Command {
 
     private $baseUrl = 'https://projecteuler.net';
 
